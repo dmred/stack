5fb95a44b80cb806b9b71272c1afd31ae9bfd694
TrigDeveloper1 <xx.artem-kokorin@mail.ru>
2016-10-21 14:11:44 +0400

    const T& instead of T& in top

--- include/stack.hpp
+++ include/stack.hpp
@@ -82,11 +82,10 @@ public:
 	auto count() const noexcept->size_t;/*noexcept*/
 	auto push(T const &)->void;/*strong*/
 	auto pop()->void;/*strong*/
-	auto top() const->T&;/*strong*/
+	auto top() const->const T&;/*strong*/
 	~stack(); 	/*noexcept*/
 	auto operator=(const stack&tmp)->stack&;/*strong*/
 	auto empty()->bool;/*noexcept*/
-
 };
 
 
