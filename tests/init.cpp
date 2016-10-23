5bdbc15b6051e8dedd9c8837d408eb0a1a9f521c
dmred <dmredchenko@gmail.com>
2016-10-23 20:46:31 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -13,41 +13,52 @@ SCENARIO("push", "[push]"){
   stack<int> s;
   s.push(1);
   REQUIRE(s.count()==1);
-  REQUIRE(s.top()==1);
-}
 
-SCENARIO("pop", "[pop]"){
-  stack<int> s;
-  s.push(1); s.pop();
-  REQUIRE(s.count()==0);
-}
-
-SCENARIO("prisv", "[prisv]"){
-  stack<int> s;
-  s.push(1);
-  stack<int> s2;
-  s2=s;
-  REQUIRE(s.count()==1);
-  REQUIRE(s.top()==1);
-}
-
-SCENARIO("cop", "[cop]"){
-  stack<int> s;
-  s.push(1);
-  stack<int> s2=s;
-  REQUIRE(s2.count()==1);
-  REQUIRE(s2.top()==1);
 }
 
 SCENARIO("top", "[top]"){
   stack<int> s;
   s.push(1);
-  REQUIRE(s.top()==1);
+  s.push(2);
+  s.push(3);
+  s.pop();
+
+  REQUIRE(s.top()==2);
+}
+SCENARIO("operprisv", "[operprisv]"){
+  stack<int> s1;
+  s1.push(1);
+  stack<int> s2;
+  s2=s1;
+  REQUIRE(s1.count()==s2.count());
+}
+
+SCENARIO("const", "[constr]"){
+  stack<int> s1;
+  s1.push(1);
+  stack<int> s2=s1;
+  REQUIRE(s1.count()==s2.count());
 }
 
 SCENARIO("empty", "[empty]"){
-  stack<int> s1, s2;
+  stack<int> s1;
   s1.push(1);
-  REQUIRE(!s1.empty());
-  REQUIRE(s2.empty());
+  REQUIRE(s1.empty()==false);
+}
+
+SCENARIO("empty2", "[empty2]"){
+  stack<int> s1;
+  s1.push(1);
+  s1.top();
+  s1.pop();
+  REQUIRE(s1.empty()==true);
+}
+
+SCENARIO("empty3", "[empty3]"){
+  stack<int> s1;
+  s1.push(1);
+  s1.push(2);
+  s1.top();
+  s1.pop();
+  REQUIRE(s1.empty()==false);
 }
