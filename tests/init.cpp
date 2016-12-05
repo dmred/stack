b4e85386531253ca7e18d542dbcfef29e9dbae72
dmred <dmredchenko@gmail.com>
2016-12-05 05:06:32 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -4,7 +4,7 @@
 #include <thread>
 #include<mutex>
 using namespace std;
-
+ 
 SCENARIO("count", "[count]"){
   stack<int> s;
   s.push(1);
@@ -15,44 +15,31 @@ SCENARIO("push", "[push]"){
   stack<int> s;
   s.push(1);
   REQUIRE(s.count()==1);
-
 }
 
-/*SCENARIO("top", "[top]"){
+SCENARIO("pop", "[pop]"){
   stack<int> s;
   s.push(1);
-  s.push(2);
-  s.push(3);
-  s.pop();
+  REQUIRE(*(s.pop())==1);	
+  REQUIRE(s.count()==0);
+}
 
-  REQUIRE(s.top()==2);
-}*/
+SCENARIO("prisv", "[prisv]"){
+  stack<int> s;
+  s.push(1);
+  stack<int> s2;
+  s2=s;
+  REQUIRE(s.count()==1);
+}
 
 SCENARIO("empty", "[empty]"){
-  stack<int> s;
-  s.push(1);
-  REQUIRE(s.empty()==false);
-}
-/*
-SCENARIO("empty2", "[empty2]"){
-  stack<int> s;
-  s.push(1);
-  s.top();
-  s.pop();
-  REQUIRE(s.empty()==true);
+  stack<int> s1, s2;
+  s1.push(1);
+  REQUIRE(!s1.empty());
+  REQUIRE(s2.empty());
 }
 
-SCENARIO("empty3", "[empty3]"){
-  stack<int> s;
-  s.push(1);
-  s.push(2);
-  s.top();
-  s.pop();
-  
-  
-  REQUIRE(s.empty()==false);
-}
-*/
+
 SCENARIO("threads", "[threads]"){
   stack<int> s;
   s.push(1);
@@ -73,43 +60,3 @@ SCENARIO("threads", "[threads]"){
 	t2.join();
   REQUIRE(s.count()==3);
 }
-
-/*
-SCENARIO("thread", "[thread]"){
-  stack<int> s;
-  s.push(3);
-  s.push(2);
-  s.push(1);
-	std::thread t1([&s](){
-		for (int i = 0; i < 5; i++) {
-			s.push(i + 4);
-		}
-	});
-	std::thread t2([&s](){
-		for (int i = 0; i < 5; i++)
-		{
-			s.pop();
-		}
-	});
-	t1.join();
-	t2.join();
-  REQUIRE(s.count()==3);
-}
-
-*/
-/*
-SCENARIO("operprisv", "[operprisv]"){
-  stack<int> s1;
-  s1.push(1);
-  stack<int> s2;
-  s2=s1;
-  REQUIRE(s1.count()==s2.count());
-}
-
-SCENARIO("const", "[constr]"){
-  stack<int> s1;
-  s1.push(1);
-  stack<int> s2=s1;
-  REQUIRE(s1.count()==s2.count());
-}
-*/
